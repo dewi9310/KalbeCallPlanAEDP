6094fea2aa270207cc4c874b891bdaf80e36a315
dewi9310 <dewioktaviani9310@gmail.com>
2019-01-25 14:04:37 +0700

    response apotek sama dokter

---
+++ app/src/main/java/com/kalbe/kalbecallplanaedp/ResponseDataJson/downloadDoterAedp/Data.java
@@ -0,0 +1,28 @@
+package com.kalbe.kalbecallplanaedp.ResponseDataJson.downloadDoterAedp;
+
+import java.util.List;
+import javax.annotation.Generated;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("com.robohorse.robopojogenerator")
+public class Data{
+
+	@SerializedName("records")
+	private List<RecordsItem> records;
+
+	public void setRecords(List<RecordsItem> records){
+		this.records = records;
+	}
+
+	public List<RecordsItem> getRecords(){
+		return records;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"Data{" + 
+			"records = '" + records + '\'' + 
+			"}";
+		}
+}
